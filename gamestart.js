f648eae3eedc5d640152c901df29a65e2a2d8d4f
marzelop <maagyo.teo23@gmail.com>
2022-05-13 00:29:45 -0300

    Add key "words" to wordbase.json

--- gamestart.js
+++ gamestart.js
@@ -1,10 +1,22 @@
 const wordState = document.querySelector("#word-state")
 const attemptList = document.querySelector("#attempt-list")
 
-let json = require("./wordbase.json")
-
-console.log(json[0])
+console.log(generateWord())
 
 function generateWord() {
-
+    let wordset, word = ""
+    fetch("https://raw.githubusercontent.com/marzelop/forca/main/wordbase.json")
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            wordset = data
+            console.log(typeof(wordset))
+            console.log(wordset)
+        })
+    
+    while (word.length < 5) {
+        word = wordset[Math.floor(Math.random() * (Object.values(wordset.length).length + 1))]
+    }
+    return word
 }
